b2d688e7ae87bc5a3f2844897b9bfb7c8444e3b2
Danny Smith <hi@danny.is>
2019-12-13 01:50:41 +0000

    Add netlify lambda for pocket items

--- .netlify/functions/pocketItems.js
+++ .netlify/functions/pocketItems.js
@@ -1,17 +1,42 @@
+const fetch = require('node-fetch')
+
 exports.handler = async (event, context, callback) => {
-  let xhr = new XMLHttpRequest()
-  xhr.open(
-    'GET',
-    'https://api.airtable.com/v0/app0sEvudHX6svP8C/Pocket%20Items'
-  )
-  xhr.setRequestHeader('Authorization', 'Bearer ' + process.env.AIRTABLE_API)
-  xhr.send()
-  xhr.onload = function() {
-    if (xhr.status == 200) {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(xhr.response),
-      })
+  const pass = (body) => {
+    //TODO: Add code here that filters and reshapes the body.
+    let returnPayload = body.records.map(function(record) {
+      return {
+        id: record.id,
+        title: record.fields['Title'],
+        url: record.fields['URL'],
+        excerpt: record.fields['Excerpt'],
+        imageUrl: record.fields['Image URL'],
+        hasImage: !record.fields['Image URL'].includes('no_image_card.png'),
+        createdAt: record.createdTime,
+        type: 'pocketItem',
+      }
+    })
+
+    callback(null, { statusCode: 200, body: JSON.stringify(returnPayload) })
+  }
+
+  try {
+    let response = await fetch(
+      'https://api.airtable.com/v0/app0sEvudHX6svP8C/Pocket%20Items',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${process.env.AIRTABLE_API}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    let data = await response.json()
+    await pass(data)
+  } catch (err) {
+    let error = {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({ error: err.message }),
     }
+    await pass(error)
   }
 }
