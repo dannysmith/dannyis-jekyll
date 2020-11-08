c0e295132a6bf2e4b91fd28558158ad4b9d1facb
Danny Smith <hi@danny.is>
2020-11-08 17:44:49 +0000

    Link directly to latest note

--- _plugins/img_tag_transform.rb
+++ _plugins/img_tag_transform.rb
@@ -3,8 +3,8 @@
 # This hook replaces markdown image links with the responsive_pnage partial, which is used by the jekyll-responsive-images plugin.
 # This saves us having to manually write {% responsive_image path: assets/my-file.jpg %} in our markdown files.
 
-Jekyll::Hooks.register [:pages, :posts], :pre_render do |post, _payload|
-  docExt = post.extname.tr('.', '')
-  post.content = post.content.gsub(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% responsive_image path: \2 alt: "\1" %}{:.image \3}')
-  post.content = post.content.gsub 'path: /', 'path: ' # you can probably optimise this a bit
-end
+# Jekyll::Hooks.register [:pages, :posts], :pre_render do |post, _payload|
+#   docExt = post.extname.tr('.', '')
+#   post.content = post.content.gsub(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% responsive_image path: \2 alt: "\1" %}{:.image \3}')
+#   post.content = post.content.gsub 'path: /', 'path: ' # you can probably optimise this a bit
+# end
